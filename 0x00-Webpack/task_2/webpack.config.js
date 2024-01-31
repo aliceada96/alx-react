e1ede691c0f8b31ae8bf0d4fc18d0501dba7e80c
aliceada96 <aliceada96@gmail.com>
2024-01-31 10:30:39 +0300

    added css and styles

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+   performance: {
+    maxAssetSize: 1000000,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        type: 'asset/resource',
+        }
+        ],
+      },
+};
