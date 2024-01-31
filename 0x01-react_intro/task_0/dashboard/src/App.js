05cdbcd275b985f93b845912ad1c4f6a4e78d08b
aliceada96 <aliceada96@gmail.com>
2024-02-01 00:08:46 +0300

    getting started with react

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import holberton from './holberton-logo.jpg';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="App-header">
+        <img src={holberton} alt="" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
