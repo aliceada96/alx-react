86bac754565467451c936017ada566f9745e6c41
aliceada96 <aliceada96@gmail.com>
2024-02-23 00:54:32 +0300

    equality

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,14 +1,5 @@
 import { is } from 'immutable';
 
 export default function areMapsEqual(map1, map2) {
-    const map1 = Map(page1);
-    const map2 = Map(page2);    
-    const mergedMap = map1.mergeWith((oldVal, newVal) => {
-      if (Map.isMap(oldVal) && Map.isMap(newVal)) {
-        return mergeDeeplyElements(oldVal.toJS(), newVal.toJS());
-      } else {
-        return newVal;
-      }
-    }, map2);   
-    return mergedMap;
+    return is(map1, map2);
 }
