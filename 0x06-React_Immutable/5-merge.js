eede21117dfeaeaa7680fc05054feb5341107b0d
aliceada96 <aliceada96@gmail.com>
2024-02-22 23:49:57 +0300

    merge and concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,7 +1,11 @@
 import { List, Map } from 'immutable';
 
-const concatElements = (page1, page2) => List(page1).concat(List(page2));
+const concatElements = (page1, page2) {
+    return List(page1).concat(List(page2));
+}
 
-const mergeElements = (page1, page2) => Map(page1).concat(Map(page2));
+const mergeElements = (page1, page2) {
+    return Map(page1).concat(Map(page2));
+}
 
 export { concatElements, mergeElements };
