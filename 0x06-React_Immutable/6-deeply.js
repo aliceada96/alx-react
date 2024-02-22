f37afc59ece311c811bf4fdf84bec995eea34e9c
aliceada96 <aliceada96@gmail.com>
2024-02-23 01:02:38 +0300

    nested merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,5 +1,5 @@
 import { Map } from 'immutable';
 
 export default function mergeDeeplyElements(page1, page2) {
-    return FileList([Map(page1), Map(page2)].flatten(true));
+    return List([Map(page1), Map(page2)].flatten(true));
 }
