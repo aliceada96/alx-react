a39d3b05da52a1b1b2f8b9c3e2305624c8637037
aliceada96 <aliceada96@gmail.com>
2024-02-22 21:18:50 +0300

    convert into immutable object

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,16 @@
+const { fromJS } = require('immutable');
+
+function getImmutableObject(object) {
+  return fromJS(object);
+}
+
+// Example usage:
+const inputObject = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+};
+
+const immutableMap = getImmutableObject(inputObject);
+console.log(immutableMap);
